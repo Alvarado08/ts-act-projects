68d22b13378695d147f7e49b16c83dcf102b2d82
Alvarado08 <jorgealvarado0238@gmail.com>
2024-01-29 19:09:43 -0600

    Functions
    Finished 01 incoming ciphers appetizer project.

---
+++ functions/secret-secrets/index.ts
@@ -0,0 +1,11 @@
+// Function Type
+export type Cipher = (char: string) => string;
+export function createCipher(cipher: Cipher){
+    return (text: string) => {
+        let result = "";
+        for(const char of text){
+            result += cipher(char);
+        }
+        return result;
+    }
+}
